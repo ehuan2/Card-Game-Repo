2a2a4325dcb19da901c1482a21ae613f4998e560
ehuan2 <47696422+ehuan2@users.noreply.github.com>
2020-02-10 11:45:51 -0500

    Update Chimp.java

--- Card-Game/src/Chimp.java
+++ Card-Game/src/Chimp.java
@@ -7,26 +7,39 @@
  * @version 1.00 2020/2/10
  */
  
-public class Chimp {
+public class CHIMP {
     
     static LinkedList<Player> players;
     static int counter = 1;
-    static Player player;
+    static int playerIndex = 0;
     
-    static int deckValue;
+    static int deckValue = 0;
     
     public static void playCard(Card aCard){ // this is playing cards
     	
-    switch (aCard.value) {
-		case 1 : break;
-		case 10 : break;
+    
+    	
+   		checkMove();
+    	
+    	
+    }
+    
+    
+    public static void checkMove(){
+    	
+    	switch (aCard.value) {
+		case 1 : 
+			deckValue += aceValue();
+			break;
+		case 10 : 
+			
+			deckValue -= 10;
+			
+			break;
 		default : 
 			deckValue += aCard.value; // nothing special, adds value
 			break;
-	}
-    	
-   	
-    	
+		}
     	
     }
     
