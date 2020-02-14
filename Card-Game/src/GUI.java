51a6197800500258d73c676a2176f7dbba5919e1
ehuan2 <47696422+ehuan2@users.noreply.github.com>
2020-02-14 11:25:33 -0500

    Update GUI.java
    Added in some basic functionailty

--- Card-Game/src/GUI.java
+++ Card-Game/src/GUI.java
@@ -2,6 +2,8 @@ public class GUI extends Application{
 
   Stage window;
   Scene scene;
+  Button[] buttonCards;
+  static int cardChoice;
   
   public GUI(){
   
@@ -9,5 +11,30 @@ public class GUI extends Application{
   
   }
   
+  public static void main(String [] args){
+    
+   launch(args);
+    
+  }
 
+  @Override 
+  public void start(Stage primaryStage) throws Exception {
+    primaryStage.setTitle("CHIMP");
+    
+    StackPane layout = new StackPane();
+    
+    buttonCards = new Button[3];
+    for(int i = 0; i < 3; i++){
+      buttonCards[i] = new Button();
+      buttonCards[i].setText("Card Choice " + (i+1));
+      
+      buttonCards[i].setOnAction(e -> cardChoice = i);
+      
+      layout.getChildren().add(buttonCards[i]);
+    }
+    
+    
+    
+  }
+  
 }
